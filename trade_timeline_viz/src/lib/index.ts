3632ed41841c910704eb1fd97cb89b35e0f5dfe7
felix <su000111@umn.edu>
2025-10-31 00:50:41 -0500

    Removed trash

--- trade_timeline_viz/src/lib/index.ts
+++ trade_timeline_viz/src/lib/index.ts
@@ -1,6 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-import Bar from './Bar.svelte'
-export {Bar}
+
 
 import LineChart from './LineChart.svelte'
 export {LineChart}
